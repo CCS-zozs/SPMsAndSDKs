12b2d1fcf7e6efa80490adf598e8eaea9bd8e10d
Jonny-CL <togyousyou@gmail.com>
2024-03-12 11:14:31 +0800

    add SDwebimage package

---
+++ SPMsAndSDKs/SDwebimageBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  SDwebimageBootcamp.swift
+//  SPMsAndSDKs
+//
+//  Created by 七つ星 on 2024/03/12.
+//
+
+import SwiftUI
+
+struct SDwebimageBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SDwebimageBootcamp()
+}
