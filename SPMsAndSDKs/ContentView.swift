1b2c6018d33821d00c6615fcbd50245de527ccba
Jonny-CL <togyousyou@gmail.com>
2024-03-12 10:59:48 +0800

    Initial Commit

---
+++ SPMsAndSDKs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SPMsAndSDKs
+//
+//  Created by 七つ星 on 2024/03/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SPMsAndSDKs/SPMsAndSDKsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPMsAndSDKsApp.swift
+//  SPMsAndSDKs
+//
+//  Created by 七つ星 on 2024/03/12.
+//
+
+import SwiftUI
+
+@main
+struct SPMsAndSDKsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
